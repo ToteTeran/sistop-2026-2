2ed0e6c54c8d8798f448bac775ef4fb518063c8c
ToteTeran <jteran19701971@gmail.com>
2026-05-17 23:08:02 -0600

    Modificación para listar los archivos del directorio

--- proyectos/SotomayorEdgar-TeranJorge/main.py
+++ proyectos/SotomayorEdgar-TeranJorge/main.py
@@ -1,5 +1,22 @@
-with open("fiunamfs.img", "rb") as disco:
-    disco.seek(5)
-    nombre = disco.read(8).decode("ascii")
+import struct
 
-print(nombre)
+CLUSTER_SIZE = 2048
+DIRECTORIO_OFFSET = CLUSTER_SIZE
+ENTRADA_SIZE = 64
+
+with open("fiunamfs.img", "rb") as disco:
+    for i in range(256):
+        offset = DIRECTORIO_OFFSET + (i * ENTRADA_SIZE)
+        disco.seek(offset)
+        entrada = disco.read(ENTRADA_SIZE)
+        tipo = chr(entrada[0])
+        
+        if tipo == "-":
+            nombre = entrada[1:16].decode("ascii").strip()
+            tamaño = struct.unpack("<I", entrada[16:20])[0]
+            cluster_inicial = struct.unpack("<I", entrada[20:24])[0]
+            
+            print("---")
+            print("Archivo:", nombre)
+            print("Tamaño:", tamaño, "bytes")
+            print("Cluster inicial:", cluster_inicial)
